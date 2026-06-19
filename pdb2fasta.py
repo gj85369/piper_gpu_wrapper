18da9c60e0e6ed239d30d9f3aeca67e3a9853cd0
gj85369 <george.jones@stonybrook.edu>
2026-06-19 10:08:10 -0400

    pdb2fasta

--- pdb2fasta.py
+++ pdb2fasta.py
@@ -16,3 +16,19 @@ bpath = Path(__file__).parent.resolve()
 cmd = f'{bpath}/pdb2fasta /home/g4code/projects/casp26/8UFO.pdb'
 
 ok = subprocess.check_output(cmd, shell=True, universal_newlines=True)
+
+def parse_pdb2fasta(pdb2fasta_results):
+    lns = []
+    for inst in pdb2fasta_results.split('\n'):
+        if len(inst) > 0:
+            lns.append(inst)
+    
+    chdic = {}
+    for i in range(0,len(lns)):
+        if lns[i][0] == '>':
+            chain_name = lns[i].split(':')[1].strip()
+            fast_seq = lns[i+1].strip()
+            chdic[chain_name] = fast_seq
+    return chdic
+
+print(parse_pdb2fasta(ok))
